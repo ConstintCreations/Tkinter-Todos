b7fc869bdf83f18d297139f961906259c42fb2fe
ConstintCreations <ethanglenn1208@gmail.com>
2026-05-09 13:42:55 -0500

    Added an add task button

--- main.py
+++ main.py
@@ -13,14 +13,16 @@ def save_tasks(tasks):
     with open(TASKS_FILE, "w") as file:
         json.dump(tasks, file, indent=4)
 
+def add_task():
+    return
+
 root = tk.Tk()
 root.title("Tkinter Todos")
 root.geometry("400x300")
 root.configure(background="#333536")
 
-title = tk.Label(root, text="Todos", font=("Arial", 24, "bold"), height=2, foreground="#f5f5f5", background="#333536")
-title.pack()
+title = tk.Label(root, text="Todos", font=("Arial", 24, "bold"), height=2, foreground="#f5f5f5", background="#333536").grid(row=0, column=1)
 
-
+button = tk.Button(root, text="+", command=add_task, foreground="#f5f5f5", background="#333536", activeforeground="#f5f5f5", activebackground="#333536", bd=0, cursor="hand2", font=("Arial", 24, "bold")).grid(row=0, column=2)
 
 root.mainloop()

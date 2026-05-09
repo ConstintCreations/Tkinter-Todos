2847e7d4d1c7c4ff7e5665ba5e337a1fe8932dbe
ConstintCreations <ethanglenn1208@gmail.com>
2026-05-09 13:51:11 -0500

    Centered title and add todo button using a frame

--- main.py
+++ main.py
@@ -20,9 +20,18 @@ root = tk.Tk()
 root.title("Tkinter Todos")
 root.geometry("400x300")
 root.configure(background="#333536")
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.columnconfigure(2, weight=1)
 
-title = tk.Label(root, text="Todos", font=("Arial", 24, "bold"), height=2, foreground="#f5f5f5", background="#333536").grid(row=0, column=1)
+titlebar = tk.Frame(root, background="#333536")
+titlebar.grid(row=0, column=1)
 
-button = tk.Button(root, text="+", command=add_task, foreground="#f5f5f5", background="#333536", activeforeground="#f5f5f5", activebackground="#333536", bd=0, cursor="hand2", font=("Arial", 24, "bold")).grid(row=0, column=2)
+
+title = tk.Label(titlebar, text="Todos", font=("Arial", 24, "bold"), height=2, foreground="#f5f5f5", background="#333536")
+title.pack(side="left")
+
+button = tk.Button(titlebar, text="+", command=add_task, foreground="#f5f5f5", background="#333536", activeforeground="#f5f5f5", activebackground="#333536", bd=0, cursor="hand2", font=("Arial", 24, "bold"))
+button.pack(side="right")
 
 root.mainloop()

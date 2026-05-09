f1cf0572a4b63d47f085e2f7da535f12e7156dbe
ConstintCreations <ethanglenn1208@gmail.com>
2026-05-09 13:30:14 -0500

    Added a title and made the background dark

--- main.py
+++ main.py
@@ -3,5 +3,11 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Tkinter Todos")
 root.geometry("400x300")
-root.mainloop()
+root.configure(background="#333536")
 
+title = tk.Label(root, text="Todos", font=("Arial", 24, "bold"), height=2, foreground="#f5f5f5", background="#333536")
+title.pack()
+
+
+
+root.mainloop()

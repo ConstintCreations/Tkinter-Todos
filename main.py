b8e4b045cf4f739b99b65c5dfe2238ac14375532
ConstintCreations <ethanglenn1208@gmail.com>
2026-05-09 14:06:55 -0500

    Added a scrollable listbox

--- main.py
+++ main.py
@@ -20,18 +20,31 @@ root = tk.Tk()
 root.title("Tkinter Todos")
 root.geometry("400x300")
 root.configure(background="#333536")
+
 root.columnconfigure(0, weight=1)
-root.columnconfigure(1, weight=1)
-root.columnconfigure(2, weight=1)
+root.rowconfigure(1, weight=1)
 
 titlebar = tk.Frame(root, background="#333536")
-titlebar.grid(row=0, column=1)
-
+titlebar.grid(row=0, column=0)
 
 title = tk.Label(titlebar, text="Todos", font=("Arial", 24, "bold"), height=2, foreground="#f5f5f5", background="#333536")
 title.pack(side="left")
 
 button = tk.Button(titlebar, text="+", command=add_task, foreground="#f5f5f5", background="#333536", activeforeground="#f5f5f5", activebackground="#333536", bd=0, cursor="hand2", font=("Arial", 24, "bold"))
 button.pack(side="right")
+
+listFrame = tk.Frame(root, background="#333536",)
+listFrame.grid(row=1, column=0, sticky="nsew")
+
+scrollbar = tk.Scrollbar(listFrame, background="#333536")
+scrollbar.pack(side="right", fill="y")
+
+listbox = tk.Listbox(listFrame, yscrollcommand=scrollbar.set, foreground="#f5f5f5", background="#333536", bd=0)
+listbox.pack(side="left", fill="both", expand=True)
+
+scrollbar.config(command=listbox.yview)
+
+for i in range(50):
+    listbox.insert(tk.END, f"Item {i+1}")
 
 root.mainloop()
